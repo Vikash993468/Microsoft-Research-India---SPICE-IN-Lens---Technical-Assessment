9b6ade6d67d9a29e27153e26ec4057a6252c8a55
Vikash Kumar <vkgupta993468@gmail.com>
2024-03-04 20:02:55 +0530

    Automation Script
    Automation Script in python to download Latest PDF

---
+++ Latest_pdf.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = Options()
+
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.ncbi.nlm.nih.gov/pmc/")
+driver.maximize_window()
+# driver.implicitly_wait(5)
+driver.find_element(By.LINK_TEXT, "Journal List").click()
+driver.find_element(By.LINK_TEXT, "AACE Clinical Case Reports").click()
+driver.find_element(By.CSS_SELECTOR, "tr:nth-child(2) > .va_middle .arc-issue").click()
+driver.find_element(By.LINK_TEXT, "PDF–198K").click()
+driver.implicity_wait(10)
+
+driver.close()
